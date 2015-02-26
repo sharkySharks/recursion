83bbe7b864ccfa6b6853424d7b76aec3396921fb
Sharky <kaydenalthen@gmail.com>
2015-02-25 23:34:14 -0600

    stringify function passing tests through first empty object

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,70 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  	
+	var g = typeof (obj);
+
+	function stringIT (arg){
+		return ''+arg;
+	}
+
+	var result = "";
+
+	if (g != 'object' || obj === null){
+		if (g == 'string'){ obj = '"'+obj+'"'}
+		result += stringIT(obj);
+	} else if (Array.isArray(obj)){
+		if (obj.length < 1){
+				obj = '['+obj+']';
+				result += stringIT(obj);
+		} else {
+			result += '[';	
+			for (var i=0; i<obj.length; i++){
+				var test = obj[i];
+				if (typeof test === 'number'){
+					result += String(test);
+				}
+				if (typeof test === 'string'){
+					result += '"'+test+'"';
+				}
+				if (Array.isArray(test)){
+					result += stringifyJSON(test);
+				}
+				if (i != obj.length-1){
+					result += ',';
+				}
+
+			}
+			result += ']';
+		}	
+		
+	} else if (typeof obj === 'object' && !(Array.isArray(obj))){
+			if (Object.keys(obj).length === 0){
+				result += '{}';
+			} else {
+				result+= '{';
+
+
+			
+				result+= '}';
+			}
+				
+
+				// if (typeof test === 'object' && !(Array.isArray(test))){
+				// 	if (Object.keys(test).length === 0){
+				// 		obj = '{'+obj+'}';
+				// 	}
+				// 	result += stringifyJSON(obj[i]);
+				// }
+
+
+	}
+				
+	return result;
+			
+	
+	
+
+	//stringifyJSON()
+
 };
